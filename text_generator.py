233a5d2e67d299fc9a9c7dc398bb8186d14787e9
june <djie627@gmail.com>
2023-03-08 19:37:41 +1100

    Stage 2/5: Break the dataset into bigrams

--- text_generator.py
+++ text_generator.py
@@ -1,4 +1,5 @@
 from nltk.tokenize import WhitespaceTokenizer
+from nltk.util import ngrams
 
 user_input = input()
 f = open(user_input, "r", encoding="utf-8")
@@ -6,10 +7,9 @@ corpus = f.read()
 
 tokenizer = WhitespaceTokenizer()
 tokens = tokenizer.tokenize(corpus)
+bigrams = list(ngrams(tokens, 2))
 
-print("Corpus statistics")
-print("All tokens: ", len(tokens))
-print("Unique tokens: ", len(set(tokens)))
+print("Number of bigrams: ", len(bigrams))
 
 while True:
     index = input()
@@ -17,10 +17,10 @@ while True:
         break
     try:
         index = int(index)
-        if index >= len(tokens):
-            print("Index Error. Please input an integer that is in the range of the corpus.")
+        if index >= len(bigrams):
+            print("Index Error. Please input a value that is not greater than the number of all bigrams.")
         else:
-            print(tokens[index])
+            print("Head: {} Tail: {}".format(bigrams[index][0], bigrams[index][1]))
     except ValueError:
         print("Type Error. Please input an integer.")
 
