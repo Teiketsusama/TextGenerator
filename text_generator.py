8f9158b41bcf197878c353cb882fe8c8929b6db9
june <djie627@gmail.com>
2023-03-11 00:33:25 +1100

    Stage 4/5: Generate random text

--- text_generator.py
+++ text_generator.py
@@ -1,15 +1,20 @@
+import random
+
 from nltk.tokenize import WhitespaceTokenizer
 from nltk.util import ngrams
 from collections import Counter
 
+# read the corpus
 user_input = input()
 f = open(user_input, "r", encoding="utf-8")
 corpus = f.read()
 
+# generate bigrams
 tokenizer = WhitespaceTokenizer()
 tokens = tokenizer.tokenize(corpus)
 bigrams = list(ngrams(tokens, 2))
 
+# generate a markov model
 head_dict = {}
 for bigram in bigrams:
     head_dict.setdefault(bigram[0], []).append(bigram[1])
@@ -17,20 +22,25 @@ for head in head_dict:
     head_dict[head] = Counter(head_dict[head])
 
 
-while True:
-    key = input()
-    if key == "exit":
-        break
-    if key not in head_dict:
-        print("Key Error. The requested word is not in the model. Please input another word.")
-    else:
-        print("Head: {}".format(key))
-        for tail in head_dict[key]:
-            print("Tail: {} Count: {}".format(tail, head_dict[key][tail]))
+# generate a sentence
+def generate_sentence():
+    sentence_list = []
+    # choose a random word as the first word of the chain
+    first_word = random.choice(list(head_dict.keys()))
+    # choose the second word based on the counts of the tails of the first word, repeat until 10 words in the chain
+    chain = random.choices(list(head_dict[first_word].keys()), weights=list(head_dict[first_word].values()), k=9)
+
+    sentence_list.append(first_word)
+    sentence_list.extend(chain)
+    sentence = " ".join(sentence_list)
+
+    return sentence
 
 
 def main():
-    pass
+    # generate 10 sentences
+    for i in range(10):
+        print(generate_sentence())
 
 
 if __name__ == '__main__':
